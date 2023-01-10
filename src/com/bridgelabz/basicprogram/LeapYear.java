a7cc3f12580d1a28a1a1e690dfd6cb2c20b3b05f
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-10 01:07:21 -0800

    Added: added leap year program

--- src/com/bridgelabz/basicprogram/LeapYear.java
+++ src/com/bridgelabz/basicprogram/LeapYear.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class LeapYear{
     static boolean isleap(int year){
-        return ((year%4==0) && (year%100!=0))||(year%400==0);
+        return ((year%4 == 0) && (year%100 != 0))||(year%400 == 0);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
