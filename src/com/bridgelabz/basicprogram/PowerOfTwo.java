480a5d638294aa0bd988ea6c9a397a8b1bc26dd8
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-10 01:08:41 -0800

    Added: added power of two program

--- src/com/bridgelabz/basicprogram/PowerOfTwo.java
+++ src/com/bridgelabz/basicprogram/PowerOfTwo.java
@@ -9,7 +9,7 @@ public class PowerOfTwo {
         } else {
             System.out.println("power of 2^" + num + "is:" + (Math.pow(2, num)));
 
-            System.out.println("printing all power value:" + num);
+            System.out.println("printing all power value=" + num);
             for (int i = 1; i <= num; i++) {
                 System.out.println("power of 2^" + i + " is:" + (Math.pow(2, i)));
             }
