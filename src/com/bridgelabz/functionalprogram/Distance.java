b703646d295c7f9402b07c810202fbf18dcbc16a
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-10 01:00:47 -0800

    Added: added basic programs and functional programs

--- src/com/bridgelabz/functionalprogram/Distance.java
+++ src/com/bridgelabz/functionalprogram/Distance.java
@@ -6,9 +6,9 @@ public class Distance {
     public static void main(String[] args) {
         System.out.println("enter the no.");
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter x value");
+        System.out.println("enter x value=");
         int x= scan.nextInt();
-        System.out.println("enter y value");
+        System.out.println("enter y value=");
         int y= scan.nextInt();
 
         double dist = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
