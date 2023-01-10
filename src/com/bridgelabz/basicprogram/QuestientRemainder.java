61e539ff5be1cae47a9f63efbfac982f08c385ba
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-10 01:12:02 -0800

    Added: added questient and remainder program

--- src/com/bridgelabz/basicprogram/QuestientRemainder.java
+++ src/com/bridgelabz/basicprogram/QuestientRemainder.java
@@ -6,8 +6,8 @@ public class QuestientRemainder {
         int dividend = 150, divisor = 7;
         int quotient = dividend / divisor;
         int remainder = dividend % divisor;
-        System.out.println("Quotient="+quotient);
-        System.out.println("Remainder="+remainder);
+        System.out.println("Quotient ="+ quotient);
+        System.out.println("Remainder ="+ remainder);
     }
 
 }
