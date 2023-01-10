8b63d25cc74544203786392d4f6b5eec55fe64dc
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-10 01:06:14 -0800

    Added: added largest no. program

--- src/com/bridgelabz/basicprogram/LargestNo.java
+++ src/com/bridgelabz/basicprogram/LargestNo.java
@@ -4,9 +4,9 @@ public class LargestNo {
     public static void main(String[] args) {
 
         int x1 = 6, x2 = 8,x3 = 7;
-        if(x1>=x2 && x1>=x3)
+        if(x1 >= x2 && x1 >= x3)
             System.out.println(x1 + "is largest no.");
-        else if(x2>=x1 && x2>=x3)
+        else if(x2 >= x1 && x2 >= x3)
             System.out.println(x2 + "is largest no.");
         else
             System.out.println(x3 + "is largest no.");
